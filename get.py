486fba00dceb44e67d0597e010f340719a78d563
Aaronblv <aaronbrian.blv14@gmail.com>
2022-04-28 19:54:56 -0300

    subiendo ejercicio get

--- get.py
+++ get.py
@@ -1,13 +1,15 @@
-import requests
+import requests 
+  
+response = requests.get('http://www.boredapi.com/api/activity?type=recreational&#39') 
+  
+body = response.json()
+print(body) 
+print(response.headers) 
 
-url="http://www.boredapi.com/api/activity?type=recreational&#39"
+cuerpo = open("cuerpo.txt", "w")
+cuerpo.write(str(body))
+cuerpo.close()
 
-r = requests.get(url)
-data = r.text
-print(data)
-
-import os
-file = open("filename.txt", "w")
-file.write({"activity":"Learn and play a new card game","type":"recreational","participants":1,"price":0,"link":"https://www.pagat.com","key":"9660022","accessibility":0} + os.linesep)
-file.write("Segunda línea")
-file.close()
+encabezado = open("encabezado.txt", "w")
+encabezado.write(str(response.headers))
+encabezado.close()
