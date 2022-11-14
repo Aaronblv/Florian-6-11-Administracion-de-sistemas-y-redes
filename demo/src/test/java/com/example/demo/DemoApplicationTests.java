7b93139ad3537779a0e1aedc61550a1c70981dee
Aaronblv <aaronbrian.blv14@gmail.com>
2022-11-14 18:12:44 -0300

    subiendo demo

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+	DemoApplication calc = new DemoApplication();
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sumar() {
+
+		assertEquals(calc.sumar(1,2),3);
+		assertNotEquals(calc.sumar(1,2),4);
+	}
+
+	@Test
+	public void shouldThrowException() {
+		Assertions.assertThrows(ArithmeticException.class, () -> {
+			calc.dividir(1, 0);
+		});
+
+	}
 }
